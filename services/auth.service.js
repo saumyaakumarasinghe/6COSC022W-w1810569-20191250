e38b29555390ccc9f0ec23c30ef33d53590ec5f2
Saumya Kumarasinghe <saumya.intern.emg@gmail.com>
2025-04-03 16:31:12 +0530

    feat: login and register endpoints

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,30 +1,61 @@
-const userDao = require("../dao/user.dao");
-const passwordService = require("./password.service");
+const { ROLE } = require('../constants');
+const userDao = require('../dao/user.dao');
+const { hashPassword, comparePassword } = require('./password.service');
 
 const login = async (req, res) => {
-    try {
-        res.send("login")
-    } catch (err) {
-        console.log(err.message);
+  try {
+    const { email, password } = req.body;
+
+    // validate request body
+    if (!email || !password) {
+      return res.status(403).json('Invalid request body properties!');
     }
-}
+
+    // check if user exists
+    const existUser = await userDao.getUserByEmail(email);
+    if (!existUser) return res.status(404).json('User not found!');
+
+    const loggedIn = await comparePassword(password, existUser.password);
+    if (!loggedIn) return res.status(500).json('Invalid request credentials!');
+
+    res.status(200).json('Login successful');
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 const register = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        //TODO validate request body
+  try {
+    let { firstName, lastName, email, mobile, password } = req.body;
 
-        const hashPassword = await passwordService.hashPassword(password);
-
-        const user = await userDao.createUser({ name, email, hashPassword });
-
-        res.send({ hi: "register", name, email, password, hashPassword, user })
-    } catch (err) {
-        console.log(err.message);
+    // validate request body
+    if (!firstName || !lastName || !email || !mobile || !password) {
+      return res.status(403).json('Invalid request body properties!');
     }
-}
+
+    // check email already exist
+    const existUser = await userDao.getUserByEmail(email);
+    if (existUser) return res.status(200).json('User already exists!');
+
+    password = await hashPassword(password);
+    const role = ROLE.USER;
+
+    const user = await userDao.createUser(
+      firstName,
+      lastName,
+      email,
+      mobile,
+      password,
+      role
+    );
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 module.exports = {
-    login,
-    register
-}
+  login,
+  register,
+};
