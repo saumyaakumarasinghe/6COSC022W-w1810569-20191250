bfda260477309d384c756ebf2f0ddb3d4724d390
saumya-kumarasinghe <saumya.20191250@iit.ac.lk>
2025-04-15 13:05:05 +0530

    chore: swagger config changes and path changes

--- server/docs/swagger.docs.js
+++ server/docs/swagger.docs.js
@@ -4,17 +4,38 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'API Documentation',
+      title: 'API Documentation for 6cosc022w-w1810569-20191250',
       version: '1.0.0',
-      description: 'This is the API documentation for your project.',
+      description:
+        'This is the API documentation for the 6cosc022w-w1810569-20191250 project.',
     },
     servers: [
       {
         url: 'http://localhost:8000', // Ensure this matches your server's base URL
       },
     ],
+    components: {
+      securitySchemes: {
+        ApiKeyAuth: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'x-api-key', // The header name for the API key
+        },
+        BearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT', // Optional: Specify the token format
+        },
+      },
+    },
+    security: [
+      {
+        ApiKeyAuth: [],
+        BearerAuth: [],
+      },
+    ],
   },
-  apis: ['./docs/api/*.yaml'], // Ensure this path matches your YAML files
+  apis: ['./docs/v1/*.yaml'], // Path to your YAML files
 };
 
 const specs = swaggerJsdoc(options);
