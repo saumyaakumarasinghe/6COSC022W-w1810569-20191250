df6bff9e98d7f3485a9dd9cddb521140254c302a
saumya-kumarasinghe <saumya.20191250@iit.ac.lk>
2025-04-16 18:58:33 +0530

    chore: format fixes

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -1,5 +1,5 @@
-import { AppSidebar } from '@/components/ui/common/app-sidebar';
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from "@/components/ui/common/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
